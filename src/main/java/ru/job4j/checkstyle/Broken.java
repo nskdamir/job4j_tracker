784bce6c627cafa78ef0c7e17215b7b07ea7f748
nskdamir <nsk.damir@gmail.com>
2023-03-11 14:09:43 +0500

    Правила Checkstyle

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -2,15 +2,12 @@ package ru.job4j.checkstyle;
 
 public class Broken {
     public static final String NEW_VALUE = "";
-
+    private int sizeOfEmpty = 10;
+    private String surname;
     private String name;
 
     public Broken() {
     }
-
-    private int sizeOfEmpty = 10;
-
-    private String surname;
 
     public void echo() {
     }
